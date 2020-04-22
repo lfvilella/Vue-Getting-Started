519d337c9b643de3c7b4a2f0d8fd7d1034ef10ca
Felipe Vilella <vilella.luisfelipe@gmail.com>
2020-04-22 12:10:14 -0300

    navigation code

--- build-a-bot/src/build/PartSelector.vue
+++ build-a-bot/src/build/PartSelector.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="[position, 'part']">
-    <img :src="selectedPart.src" title="arm"/>
+    <img @click="showPartInfo()" :src="selectedPart.src" title="arm"/>
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
     <span class="sale" v-show="selectedPart.onSale">Sale!</span>
@@ -51,6 +51,10 @@ export default {
   },
 
   methods: {
+    showPartInfo() {
+      this.$router.push('/parts');
+    },
+
     emitSelectedPart() {
       this.$emit('partSelected', this.selectedPart);
     },
@@ -98,6 +102,7 @@ export default {
 }
 .part img {
   width:165px;
+  cursor: pointer;
 }
 .top {
   border-bottom: none;
