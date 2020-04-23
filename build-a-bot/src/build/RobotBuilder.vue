c6377af93a6b27c8c46ac9ee1c3658598ceb82d3
Felipe Vilella <vilella.luisfelipe@gmail.com>
2020-04-23 16:45:23 -0300

    modules and namespaced

--- build-a-bot/src/build/RobotBuilder.vue
+++ build-a-bot/src/build/RobotBuilder.vue
@@ -60,7 +60,7 @@ import CollapsibleSection from '../shared/CollapsibleSection.vue';
 export default {
   name: 'RobotBuilder',
   created() {
-    this.$store.dispatch('getParts');
+    this.$store.dispatch('robots/getParts');
   },
   beforeRouteLeave(to, from, next) {
     if (this.addedToCart) {
@@ -89,7 +89,7 @@ export default {
 
   computed: {
     avaliableParts() {
-      return this.$store.state.parts;
+      return this.$store.state.robots.parts;
     },
 
     saleBorderClass() {
@@ -114,7 +114,7 @@ export default {
         + robot.torso.cost
         + robot.base.cost;
 
-      this.$store.dispatch('addRobotToCart', { ...robot, cost })
+      this.$store.dispatch('robots/addRobotToCart', { ...robot, cost })
         .then(() => this.$router.push('/cart'));
       this.addedToCart = true;
     },
